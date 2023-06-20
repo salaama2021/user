9ccb34666356039bd87b6f271ff631ee0eca14c7
salaama2021 <134949692+salaama2021@users.noreply.github.com>
2023-06-20 16:28:59 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import React from 'react'
-import{ BrowserRouter, Routes, Route } from 'react-router-dom'
-import Make from './Make'
-import Home from './Home'
-import Update from './Update'
+import React from 'react';
+import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/make' element={<make/>}></Route>
-      <Route path='/update/:id' element={<Update/>}></Route>
-      <Route path='/read/:id' element={<Read/>}></Route>
-    </Routes>
-    </BrowserRouter>
-  )
+    <div className='todo-app'>
+      <TodoList />
+    </div>
+  );
 }
 
-export default App
+export default App;
